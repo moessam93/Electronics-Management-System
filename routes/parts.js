9e32a3eeb763e919146a79a98b93e5a2a1440951
Mohamed Essam <moessam@Mohameds-Air.home>
2022-02-02 18:19:40 +0200

    add /api

--- routes/parts.js
+++ routes/parts.js
@@ -1,39 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { addTaxonomy,
-    getTaxonomy,
-    updateTaxonomy,
-    deleteTaxonomy,
-    addPackage,
-    getPackage,
-    updatePackage,
-    deletePackage,
-    addPrice,
-    getPrice,
-    updatePrice,
-    deletePrice,
-    addStock,
-    getStock,
-    updateStock,
-    deleteStock,
-    addSupplier,
-    getSupplier,
-    updateSupplier,
-    deleteSupplier,
-    addStatus,
-    getStatus,
-    updateStatus,
-    deleteStatus,
-    exportParts,
-} = require('../controllers/parts');
 
-router.route('/taxonomy').post(addTaxonomy).get(getTaxonomy).patch(updateTaxonomy).delete(deleteTaxonomy);
-router.route('/package').post(addPackage).get(getPackage).patch(updatePackage).delete(deletePackage);
-router.route('/price').post(addPrice).get(getPrice).patch(updatePrice).delete(deletePrice);
-router.route('/stock').post(addStock).get(getStock).patch(updateStock).delete(deleteStock);
-router.route('/supplier').post(addSupplier).get(getSupplier).patch(updateSupplier).delete(deleteSupplier);
-router.route('/status').post(addStatus).get(getStatus).patch(updateStatus).delete(deleteStatus);
-router.route('/export_parts').get(exportParts);
+const {addTaxonomy,getTaxonomy,updateTaxonomy,deleteTaxonomy} = require('../controllers/taxonomy');
+const {addPackage,getPackage,updatePackage,deletePackage} = require('../controllers/package');
+const {addPrice,getPrice,updatePrice,deletePrice} = require('../controllers/price');
+const {addStatus,getStatus,updateStatus,deleteStatus} = require('../controllers/status');
+const {addStock,getStock,updateStock,deleteStock} = require('../controllers/stock');
+const {addSupplier,getSupplier,updateSupplier,deleteSupplier} = require('../controllers/supplier');
+const {exportParts} = require('../controllers/parts');
+
+router.route('/api/taxonomy').post(addTaxonomy).get(getTaxonomy).patch(updateTaxonomy).delete(deleteTaxonomy);
+router.route('/api/package').post(addPackage).get(getPackage).patch(updatePackage).delete(deletePackage);
+router.route('/api/price').post(addPrice).get(getPrice).patch(updatePrice).delete(deletePrice);
+router.route('/api/stock').post(addStock).get(getStock).patch(updateStock).delete(deleteStock);
+router.route('/api/supplier').post(addSupplier).get(getSupplier).patch(updateSupplier).delete(deleteSupplier);
+router.route('/api/status').post(addStatus).get(getStatus).patch(updateStatus).delete(deleteStatus);
+router.route('/api/export_parts').get(exportParts);
 
 router.route('/upload/add-taxonomy').post(addTaxonomy);
 router.route('/upload/add-package').post(addPackage);
