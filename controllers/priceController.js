be5b0fe5b419e1e3744d75159a742357d4df40b6
moessam <mhran9333@gmail.com>
2023-12-10 03:36:46 +0200

    code enhancements and use price related DTOs

--- controllers/priceController.js
+++ controllers/priceController.js
@@ -1,5 +1,7 @@
 // controllers/priceController.js
+
 const priceService = require('../services/priceService');
+const priceDTOs = require('../DTOs/price/priceDTOs');
 
 const addPrice = (req, res) => {
     if (req.body.path) {
@@ -23,33 +25,61 @@ const addPrice = (req, res) => {
         });
     }
 };
+
 /**
  * @swagger
  * /api/price/all:
  *   get:
- *     summary: Get a list of parts prices
- *     description: Retrieve a list of parts prices from the database.
+ *     summary: Get a paginated list of parts prices
+ *     tags: 
+ *          - Price
+ *     description: Retrieve a paginated list of parts prices from the database.
+ *     parameters:
+ *       - in: query
+ *         name: pageSize
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: The number of items to return per page.
+ *       - in: query
+ *         name: pageNum
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: The page number to retrieve.
+ *       - in: query
+ *         name: searchKeyword
+ *         schema:
+ *           type: string
+ *         description: Optional keyword to search for in part numbers.
  *     responses:
  *       200:
- *         description: A list of parts prices.
+ *         description: A paginated list of parts prices.
+ *       400:
+ *         description: Bad request. Invalid pageSize or pageNum.
  *       500:
  *         description: Internal server error.
  */
 const getAllPrices = (req, res) => {
-        priceService.getAllPrices()
-            .then((result) => res.status(200).json(result))
-            .catch((error) => res.status(500).send(error.message));
-};
+    const { pageSize, pageNum, searchKeyword } = req.query;
+    const requestDto = new priceDTOs.GetAllPricesRequestDto(pageNum, pageSize, searchKeyword);
+
+    priceService.getAllPrices(requestDto)
+      .then((result) => res.status(200).json(result))
+      .catch((error) => res.status(500).send(error.message));
+  };
 
 /**
  * @swagger
- * /api/price/{part_number}:
+ * /api/price/{partNumber}:
  *   get:
  *     summary: Get a part price
+ *     tags: 
+ *          - Price
  *     description: Retrieve a part price from the database.
  *     parameters:
  *       - in: path
- *         name: part_number
+ *         name: partNumber
  *         schema:
  *           type: string
  *         required: true
@@ -62,11 +92,14 @@ const getAllPrices = (req, res) => {
  *       500:
  *         description: Internal server error.
  */
-const getPriceByPartNumber = (req,res) => { 
-        priceService.getPriceByPartNumber(req.params.part_number)
+const getPriceByPartNumber = (req,res) => {
+    const partNumber = req.params.partNumber;
+    const requestDto = new priceDTOs.GetSinglePartPriceRequestDto(partNumber);
+
+        priceService.getPriceByPartNumber(requestDto)
             .then((result) => {
-                if (!result.length) {
-                    res.status(404).send(`Part Number ${req.params.part_number} not found`);
+                if (!(result && result.partNumber && result.price)) {
+                    res.status(404).send(`Part Number ${requestDto.partNumber} not found`);
                 } else {
                     res.status(200).json(result);
                 }
--- services/priceService.js
+++ services/priceService.js
@@ -1,6 +1,7 @@
 // services/priceService.js
 const csvtojson = require('csvtojson');
 const emsDB = require('../config/mySqlDB');
+const priceDTOs = require('../DTOs/price/priceDTOs');
 
 const addPricesFromCSV = async (filePath) => {
     return new Promise((resolve, reject) => {
@@ -18,26 +19,50 @@ const addPricesFromCSV = async (filePath) => {
     });
 };
 
-const getAllPrices = async () => {
+const getAllPrices = async (getAllPricesRequestDto) => {
     return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM price`;
-        emsDB.query(sql, (err, result) => {
+        const sql = `
+        SELECT SQL_CALC_FOUND_ROWS part_number, price FROM price 
+        WHERE part_number LIKE '%${getAllPricesRequestDto.searchKeyword}%' 
+        LIMIT ${getAllPricesRequestDto.pageSize} OFFSET ${getAllPricesRequestDto.pageSize * (getAllPricesRequestDto.pageNum - 1)};
+        SELECT FOUND_ROWS() as totalRecords;`;
+
+        emsDB.query(sql, [getAllPricesRequestDto.searchKeyword, 
+            getAllPricesRequestDto.pageSize,
+            getAllPricesRequestDto.pageSize * (getAllPricesRequestDto.pageNum - 1)], (err, result) => {
             if (err) {
+                console.error('Error executing MySQL query:', err);
                 reject(err);
             }
-            resolve(result);
+            else {
+                // Process the result as needed
+                const data = result[0];
+                const totalRecords = result[1][0].totalRecords;
+                const response = new priceDTOs.GetAllPricesResponseDto(totalRecords, data);
+                resolve(response);
+            }
         });
     });
 };
 
-const getPriceByPartNumber = async (partNumber) => {
+const getPriceByPartNumber = async (getSinglePartPriceRequestDto) => {
     return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM price WHERE part_number="${partNumber}"`;
+        const sql = `SELECT * FROM price WHERE part_number="${getSinglePartPriceRequestDto.partNumber}"`;
         emsDB.query(sql, (err, result) => {
             if (err) {
+                console.error('Error executing MySQL query:', err);
                 reject(err);
             }
-            resolve(result);
+            else{
+                let response;
+                if (result.length > 0){
+                    response = new priceDTOs.GetSinglePartPriceResponseDto(result[0].part_number, result[0].price);
+                }
+                else{
+                    response = null;
+                }
+                resolve(response);
+            }
         });
     });
 };
